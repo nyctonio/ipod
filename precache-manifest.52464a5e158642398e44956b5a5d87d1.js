4016b1edc9e232ff88913077355fef866a61b8b3
nyctonio <nyctonio.dev@gmail.com>
2021-05-19 20:35:02 +0530

    Updates

---
+++ precache-manifest.52464a5e158642398e44956b5a5d87d1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b827be588316272196d7b50f6d9c4c",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "30c52f9f3221b6ea3a3b",
+    "url": "/ipod/static/css/main.2b0c91fb.chunk.css"
+  },
+  {
+    "revision": "a14eb1d4540fd6440716",
+    "url": "/ipod/static/js/2.c13b7da9.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/ipod/static/js/2.c13b7da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c52f9f3221b6ea3a3b",
+    "url": "/ipod/static/js/main.acdb3518.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  },
+  {
+    "revision": "092c7d83566618c649286ba2531d1171",
+    "url": "/ipod/static/media/AudioTapes.092c7d83.png"
+  },
+  {
+    "revision": "a4aaa9f22129f50f82fd71023e44cd40",
+    "url": "/ipod/static/media/BlueCubes.a4aaa9f2.png"
+  },
+  {
+    "revision": "5bb3fcdf00d62055bf72f743161dc3ea",
+    "url": "/ipod/static/media/BlueJelly.5bb3fcdf.png"
+  },
+  {
+    "revision": "941e51f3f98607cdb5ac3135f29024f7",
+    "url": "/ipod/static/media/game.941e51f3.jpg"
+  },
+  {
+    "revision": "7cf5bdd0754de07cd03dbbc091894b8c",
+    "url": "/ipod/static/media/pic1.7cf5bdd0.png"
+  },
+  {
+    "revision": "474c759bbaea1be73e9c26ee3fd2ca2d",
+    "url": "/ipod/static/media/pic2.474c759b.png"
+  },
+  {
+    "revision": "af3bd9a6c5d0ee8b31560649c03b9786",
+    "url": "/ipod/static/media/pic3.af3bd9a6.png"
+  },
+  {
+    "revision": "9e14b27ec5327891f88904777d8294db",
+    "url": "/ipod/static/media/pic4.9e14b27e.png"
+  },
+  {
+    "revision": "9bf4a79ab74027c960a3dded0a406185",
+    "url": "/ipod/static/media/pic5.9bf4a79a.png"
+  },
+  {
+    "revision": "994f8bd3084b260278068de214632e59",
+    "url": "/ipod/static/media/settings.994f8bd3.png"
+  },
+  {
+    "revision": "b3aa510a14fdfdeef9839979f2f77c0a",
+    "url": "/ipod/static/media/song1.b3aa510a.mp3"
+  },
+  {
+    "revision": "b2615f33402b4f19888e46edc9278340",
+    "url": "/ipod/static/media/song2.b2615f33.mp3"
+  },
+  {
+    "revision": "2d7b0a24694f3a5137e254693a2ece5d",
+    "url": "/ipod/static/media/song3.2d7b0a24.mp3"
+  },
+  {
+    "revision": "d2fc5263b47bb2724c7c3892bb8a7d40",
+    "url": "/ipod/static/media/song4.d2fc5263.mp3"
+  },
+  {
+    "revision": "4a46b26ff3f99d550420d5d5e16a105c",
+    "url": "/ipod/static/media/song5.4a46b26f.mp3"
+  }
+]);
